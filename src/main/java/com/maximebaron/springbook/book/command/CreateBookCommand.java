3fbabe0efd240b8e15859176293a53d0389acb0a
Maxime Baron <maxime.baron.dev@gmail.com>
2026-03-25 22:07:49 +0100

    test(book): add unit tests for BookService

--- src/main/java/com/maximebaron/springbook/book/command/CreateBookCommand.java
+++ src/main/java/com/maximebaron/springbook/book/command/CreateBookCommand.java
@@ -3,9 +3,11 @@ package com.maximebaron.springbook.book.command;
 import com.maximebaron.springbook.book.BookFormat;
 import com.maximebaron.springbook.book.BookGenre;
 import jakarta.validation.constraints.*;
+import lombok.Builder;
 
 import java.time.LocalDate;
 
+@Builder
 public record CreateBookCommand(
         String isbn,
         @NotBlank String title,
--- src/main/java/com/maximebaron/springbook/book/command/UpdateBookCommand.java
+++ src/main/java/com/maximebaron/springbook/book/command/UpdateBookCommand.java
@@ -3,12 +3,12 @@ package com.maximebaron.springbook.book.command;
 import com.maximebaron.springbook.book.BookFormat;
 import com.maximebaron.springbook.book.BookGenre;
 import jakarta.validation.constraints.*;
+import lombok.Builder;
 
 import java.time.LocalDate;
 
+@Builder
 public record UpdateBookCommand(
-        String isbn,
-
         @Size(min = 1)
         String title,
 
